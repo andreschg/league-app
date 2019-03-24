415794e45ebbc7eb442e02f66b766d6ea8bf8937
Oscar Chaves <andresch1009@gmail.com>
2019-03-23 19:51:14 -0600

    Basic configuration finished.

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import AppBar from '@material-ui/core/AppBar';
 import './styles/styles.scss';
 
 const App = () => (
   <div>
+    <AppBar />
     <h1>Application</h1>
   </div>
 );
