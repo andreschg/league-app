404d81d42c1b46566276d963b3126877e656eaa4
Oscar Chaves <andresch1009@gmail.com>
2019-04-09 20:35:58 -0600

    Simple login page example.

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 // import AppBar from '@material-ui/core/AppBar';
+import CssBaseLine from '@material-ui/core/CssBaseline';
 import { Login } from './componets/Login/Login';
 import './styles/styles.scss';
 
 const App = () => (
   <div>
+    <CssBaseLine />
     <Login classes="league-app"/>
   </div>
 );
