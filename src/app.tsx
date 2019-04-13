11f2c486ee902bb9d5278e7873940afc012766f5
Oscar Chaves <andresch1009@gmail.com>
2019-04-13 17:33:13 -0600

    Added basic firebase implementation.

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom';
 import CssBaseLine from '@material-ui/core/CssBaseline';
 import { Login } from './componets/Login/Login';
 import './styles/styles.scss';
+// import './firebase/firebase.js';
 
 const App = () => (
   <div>
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
 
+require('dotenv').config({path: '.env.development'});
 module.exports = () => {
   return {
     entry: './src/app.tsx',
@@ -67,6 +68,14 @@ module.exports = () => {
       new webpack.ProvidePlugin({
         $: 'jquery',
         jQuery: 'jquery',
+      }),
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
       })
     ],
     devtool: 'inline-source-map',
